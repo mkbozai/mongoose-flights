7ccbf8e31376c3443995eb6cfcd5d0e196236951
mkbozai <munaaf24@gmail.com>
2022-07-18 18:24:41 -0500

    part 1 finished

--- controllers/flights.js
+++ controllers/flights.js
@@ -8,12 +8,18 @@ module.exports = {
 
 function index(req, res) {
     Flight.find({}, function(err, flights) {
-        res.render('flights/index', { title: 'All Flights', flights })
+        res.render('flights/index', { flights })
     })
 }
 
 function newFlight(req, res) {
-    res.render('flights/new', { title: 'Add Flight' });
+    const newFlight = new Flight();
+// Obtain the default date
+const dt = newFlight.departs;
+// Format the date for the value attribute of the input
+let departsDate = `${dt.getFullYear()}-${(dt.getMonth() + 1).toString().padStart(2, '0')}`;
+departsDate += `-${dt.getDate().toString().padStart(2, '0')}T${dt.toTimeString().slice(0, 5)}`;
+res.render('flights/new', { departsDate });
 }
 
 function create(req, res) {
--- models/flight.js
+++ models/flight.js
@@ -3,10 +3,28 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const flightSchema = new Schema({
-    airline: String,
-    airport: String,
-    flightNo: Number,
-    departs: Date,
+    airline: {
+        type: String,
+        enum: ['American', 'Southwest', 'United']
+    },
+    airport: {
+        type: String,
+        enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'],
+        default: 'DEN'
+    },
+    flightNo: {
+        type: Number,
+        required: true,
+        min: 10,
+        max: 9999,
+    },
+    departs: {
+        type: Date,
+        default: function() {
+            let today = new Date();
+            return today.setFullYear(today.getFullYear() + 1)
+        }
+    }
 });
 
 module.exports = mongoose.model('Flight', flightSchema);
